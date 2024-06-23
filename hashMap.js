0d88273827c4ef2a3f13080a2b090d498366eb21
404CoderM <117585798+404CoderM@users.noreply.github.com>
2024-06-23 22:41:18 +0530

    hash initialization

---
+++ hashMap.js
@@ -0,0 +1,17 @@
+class HashMap {
+  constructor(initialCapacity = 16, loadFactor = 0.75) {
+    this.buckets = new Array(initialCapacity);
+    this.loadFactor = loadFactor;
+    this.size = 0;
+    this.collisions = 0;
+  }
+
+  hash(key) {
+    let hashCode = 0;
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode += primeNumber + key.charCodeAt(i);
+    }
+    return hashCode % this.buckets.length;
+  }
+}
