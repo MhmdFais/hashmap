7013052afcb4486eb42aeb79312a13125b8f7107
404CoderM <117585798+404CoderM@users.noreply.github.com>
2024-06-24 10:51:15 +0530

    final changes to the hashMap.js

--- hashMap.js
+++ hashMap.js
@@ -1,6 +1,6 @@
 class HashMap {
   constructor(initialCapacity = 16, loadFactor = 0.75) {
-    this.buckets = new Array(initialCapacity);
+    this.buckets = new Array(initialCapacity).fill(null).map(() => []);
     this.loadFactor = loadFactor;
     this.size = 0;
   }
@@ -17,7 +17,7 @@ class HashMap {
   set(key, value) {
     const index = this.hash(key);
     const bucket = this.buckets[index];
-    const existingNode = bucket?.find((node) => node.key === key);
+    const existingNode = bucket.find((node) => node.key === key);
 
     if (existingNode) {
       existingNode.value = value;
@@ -27,30 +27,73 @@ class HashMap {
     }
 
     if (this.size / this.buckets.length > this.loadFactor) {
-      //this.resize();
+      this.resize();
     }
   }
 
   get(key) {
     const index = this.hash(key);
     const bucket = this.buckets[index];
-    const existingNode = bucket?.find((node) => node.key === key);
+    const existingNode = bucket.find((node) => node.key === key);
     return existingNode?.value;
   }
 
   has(key) {
     const index = this.hash(key);
-    if (!this.buckets[index]) return false;
-    else return true;
+    const bucket = this.buckets[index];
+    return bucket.find((node) => node.key === key) ? true : false;
   }
 
   remove(key) {
     const index = this.hash(key);
     const bucket = this.buckets[index];
-    const existingNode = bucket?.find((node) => node.key === key);
+    const existingNode = bucket.find((node) => node.key === key);
     if (existingNode) {
       bucket.remove(existingNode);
       this.size--;
     }
+  }
+
+  length() {
+    return this.size;
+  }
+
+  clear() {
+    this.buckets = new Array(this.buckets.length).fill(null).map(() => []);
+    this.size = 0;
+  }
+
+  entries() {
+    let entries = [];
+    this.buckets.forEach((bucket) => {
+      if (bucket) {
+        bucket.forEach((node) => {
+          entries.push([node.key, node.value]);
+        });
+      }
+    });
+    return entries;
+  }
+
+  resize() {
+    const newCapacity = this.buckets.length * 2;
+    const newBuckets = new Array(newCapacity).fill(null).map(() => []);
+
+    this.buckets.forEach((bucket) => {
+      if (bucket) {
+        bucket.forEach((node) => {
+          const index = this.hash(node.key);
+          const newBucket = newBuckets[index];
+          if (newBucket) {
+            newBucket.append(node);
+          } else {
+            newBuckets[index] = new LinkedList();
+            newBuckets[index].append(node);
+          }
+        });
+      }
+    });
+
+    this.buckets = newBuckets;
   }
 }
